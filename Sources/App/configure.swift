9d8d93a5ccd7acd2048cb502b80bb38cf1920634
Matt Wylder <mwylder@paypal.com>
2022-06-03 13:15:01 -0500

    added image. up tp page 240

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,8 +5,7 @@ import Leaf
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
     if app.environment == .testing {
         try FileManager.default.removeItem(atPath: "testdb.sqlite")
